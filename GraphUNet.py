e6c086cf8c9483d30c8b3d0581ed61e3bd2d5974
Dimitri Coukos <dimitri.coukos@epfl.ch>
2020-01-08 14:07:54 +0100

    write script to call GraphUNet

---
+++ GraphUNet.py
@@ -0,0 +1,145 @@
+import torch
+import numpy as np
+from torch.nn.functional import binary_cross_entropy
+from torch_geometric.data import DataLoader
+from torch_geometric.transforms import FaceToEdge
+from torch_geometric.utils import precision, recall, f1_score
+from dataset import Structures, MiniStructures
+from torch.utils.tensorboard import SummaryWriter
+import datetime
+from torch_geometric.nn import GraphUNet
+from utils import generate_weights, perf_measure
+'''
+baseline.py implements a baseline model. Experiment using pytorch-geometric
+    and FeaStNet.
+'''
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+samples = 50  # Doesn't currently do anything.
+if str(device) == 'cuda':
+    epochs = 600
+else:
+    epochs = 10
+batch_size = 10
+validation_split = .2
+shuffle_dataset = False
+random_seed = 42
+dropout = False  # too much dropout?
+learning_rate = .001
+lr_decay = 0.99
+weight_decay = 1e-4
+
+dataset = MiniStructures(root='./datasets/mini_pos/', pre_transform=FaceToEdge())
+# Add momentum? After a couple epochs, gradients locked in at 0.
+samples = len(dataset)
+if shuffle_dataset:
+    dataset = dataset.shuffle()
+n_features = dataset.get(0).x.shape[1]
+
+
+model = GraphUNet(n_features, 100, 1, 10).to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+
+writer = SummaryWriter(comment='model:{}_lr:{}_dr:{}_sh:{}'.format(str(type(model)).split('.')[1].split("\'")[0],
+                                                                   learning_rate,
+                                                                   dropout,
+                                                                   shuffle_dataset))
+
+cutoff = int(np.floor(samples*(1-validation_split)))
+train_dataset = dataset[:cutoff]
+test_dataset = dataset[cutoff:]
+
+
+train_loader = DataLoader(train_dataset, shuffle=shuffle_dataset, batch_size=batch_size)
+test_loader = DataLoader(test_dataset, shuffle=False, batch_size=len(test_dataset))
+
+
+# Notes on training:
+# The size of the input matrix = [n_nodes, n_x(explicit features)]
+# rotate structures at each epoch
+# converter = rotations
+
+# to avoid continuous reloading
+test_data = next(iter(test_loader))
+test_labels = test_data.y.to(device)
+
+# Adding graph to Tensorboard
+datapoint = dataset.get(0)
+x, edge_index = datapoint.x.to(device), datapoint.edge_index.to(device)
+labels = datapoint.y.to(device)
+
+# writer.add_graph(model, input_to_model=(x, edge_index, labels))
+
+# previous loss stored for adaptive learning rate.
+tr_loss = 1
+prev_loss = 1
+
+for epoch in range(1, epochs+1):
+    # rotate the structures between epochs
+    model.train()
+    # dataset_ = [converter(structure) for structure in dataset]
+    first_batch_labels = torch.Tensor()
+    pred = torch.Tensor()
+    if prev_loss < tr_loss:  # adaptive learning rate.
+        for g in optimizer.param_groups:
+            learning_rate = learning_rate*lr_decay
+            g['lr'] = learning_rate
+    prev_loss = tr_loss
+    for batch_n, data in enumerate(train_loader):
+        optimizer.zero_grad()
+        x, edge_index = data.x.to(device), data.edge_index.to(device)
+        labels = data.y.to(device)
+        inter = model(x, edge_index, labels)
+        out = torch.sigmoid(inter)
+        tr_loss = binary_cross_entropy(out, labels, weights=generate_weights())
+        tr_loss.backward()
+        optimizer.step()
+        if batch_n == 0:
+            first_batch_labels = data.y.clone().detach().to(device)
+            pred = out.clone().detach().round().to(device)
+
+    print("---- Round {}: loss={:.4f} lr:{:.6f}".format(epoch, tr_loss, optimizer.param_groups[0]['lr']))
+
+    #  --------------  REPORTING ------------------------------------
+
+    (train_TP, train_FP, train_TN, train_FN) = perf_measure(pred, first_batch_labels)
+    print("Performance measures: {} {} {} {}".format(train_TP, train_FP, train_TN, train_FN))
+    # print(stats(last_batch_labels, pred))
+    train_precision = precision(pred, first_batch_labels, 2)[1].item()
+    train_recall = recall(pred, first_batch_labels, 2)[1].item()
+    train_f1 = f1_score(pred, first_batch_labels, 2)[1].item()
+
+    model.eval()
+    x, edge_index = test_data.x.to(device), test_data.edge_index.to(device)
+    labels = test_data.y.to(device)
+    te_loss, out = model(x, edge_index, labels)
+    pred = out.detach().round().to(device)
+
+    (test_TP, test_FP, test_TN, test_FN) = perf_measure(pred, test_labels)
+    test_precision = precision(pred, test_labels, 2)[1].item()
+    test_recall = recall(pred, test_labels, 2)[1].item()
+    test_f1 = f1_score(pred, test_labels, 2)[1].item()
+
+    writer.add_scalars('True positive rate', {'train': train_TP,
+                                              'test': test_TP}, epoch)
+    writer.add_scalars('False positive rate', {'train': train_FP,
+                                               'test': test_FP}, epoch)
+    writer.add_scalars('True negative rate', {'train': train_TN,
+                                              'test': test_TN}, epoch)
+    writer.add_scalars('False negative rate', {'train': train_FN,
+                                               'test': test_FN}, epoch)
+    writer.add_scalars('Recall', {'train': train_recall,
+                                  'test': test_recall}, epoch)
+    writer.add_scalars('Precision', {'train': train_precision,
+                                     'test': test_precision}, epoch)
+    writer.add_scalars('F1_score', {'train': train_f1,
+                                    'test': test_f1}, epoch)
+    writer.add_scalars('Loss', {'train': tr_loss,
+                                'test': te_loss}, epoch)
+
+writer.close()
+
+now = datetime.datetime.now().strftime('%y%m%d%H%M')
+torch.save(model.state_dict(), 'models/{}_{}.pt'.format(str(model).split('(')[0], now))
