141b91f43349ae599a9f28ae9b808bae880624b7
coukos <dimitri.coukos@epfl.ch>
2020-01-15 12:11:12 +0100

    run unnamed model 10, with 2 fewer fc layers

--- params.py
+++ params.py
@@ -8,9 +8,5 @@ learn_rate = .001
 lr_decay = 0.99
 weight_decay = 1e-4
 epochs = 200
-<<<<<<< HEAD
 version = '-'
-=======
-version = '0.10d'
->>>>>>> 1c65f985a85ae4a504c8c3371cc95225a6ffe7ec
 suppress_warnings = True
