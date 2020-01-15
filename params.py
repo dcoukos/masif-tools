c326aa503b047fed739a31fb54fcffb36fb56257
coukos <dimitri.coukos@epfl.ch>
2020-01-15 14:45:36 +0100

    add runs model v.10 & v.11_

--- params.py
+++ params.py
@@ -1,10 +1,10 @@
 # REMEMBER TO CHANGE VERSION NUMBER
-batch_size = 10
+batch_size = 30
 validation_split = .2
 shuffle_dataset = False
 random_seed = 42
 dropout = False  # too much dropout?
-learn_rate = .001
+learn_rate = .01
 lr_decay = 0.99
 weight_decay = 1e-4
 epochs = 4000
