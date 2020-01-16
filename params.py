82a3ecddd7fa7c36d5672581a39a2e846daf8a04
coukos <dimitri.coukos@epfl.ch>
2020-01-16 15:06:25 +0100

    add runs 8c

--- params.py
+++ params.py
@@ -2,11 +2,11 @@
 batch_size = 10
 validation_split = .2
 shuffle_dataset = True
-random_seed = 42
+random_seed = 15
 dropout = True  # too much dropout?
 learn_rate = .007
 lr_decay = 0.99
 weight_decay = 1e-4
 epochs = 500
-version = '8c'
+version = '9h'
 suppress_warnings = True
