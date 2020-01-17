930ea46e9bd35ba47cd626bf2447476775012f8e
coukos <dimitri.coukos@epfl.ch>
2020-01-17 13:58:20 +0100

    update parameters

--- params.py
+++ params.py
@@ -1,16 +1,16 @@
 # REMEMBER TO CHANGE VERSION NUMBER
-batch_size = 10
-test_batch_size = 10
+batch_size = 100
+test_batch_size = 100
 validation_split = .2
 shuffle_dataset = False
 random_seed = 37
 dropout = True  # too much dropout?
-learn_rate = .04
-intermediate_learn_rate = 0.02
+learn_rate = .004
+intermediate_learn_rate = 0.002
 lr_decay = 0.3
 lr_cap_decay = 0.7
 patience = 10
 weight_decay = 1e-4
-epochs = 2000
-version = '14'
+epochs = 3000
+version = '11c'
 suppress_warnings = True
