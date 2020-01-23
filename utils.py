bfc4276e7bbde95aac50a7f42dc6d06797eb6e4c
coukos <dimitri.coukos@epfl.ch>
2020-01-23 14:15:26 +0100

    debugging path issues

--- utils.py
+++ utils.py
@@ -80,14 +80,6 @@ def generate_example_surfaces(model_type, model_path, n_examples=5, use_structur
         structures through the model, and saves the graph vertices with the predicted surface
         interface labels.
     '''
-    import torch
-    import params as p
-    from torch_geometric.transforms import FaceToEdge
-    from glob import glob
-    from dataset import read_ply
-    model_type = p.model_type
-    model_path = './models/Jan23_13:53_15a/final.pt'
-
     converter = FaceToEdge()
 
     paths = glob('./structures/test/*')[:n_examples]
