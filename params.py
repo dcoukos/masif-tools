47fec17046d3177eae6e95484d52c4a4aec18412
coukos <dimitri.coukos@epfl.ch>
2020-01-23 14:01:06 +0100

    debugging

--- params.py
+++ params.py
@@ -1,8 +1,8 @@
 # REMEMBER TO CHANGE VERSION NUMBER
 from models import ThreeConvGlobal, SixConv, SixConvPassThrough, SixConvPT_LFC, SixConvResidual
 
-batch_size = 80
-test_batch_size = 80
+batch_size = 20
+test_batch_size = 20
 validation_split = .2
 shuffle_dataset = False
 random_seed = 37
@@ -13,10 +13,11 @@ lr_decay = 0.3
 lr_cap_decay = 0.7
 patience = 10
 weight_decay = 0
-epochs = 3000
-version = '13i'
+epochs = 2
+version = '15a'
 suppress_warnings = True
-dataset = 'full'
+dataset = 'thous'
 model_type = SixConvResidual
 interface_weight = .8
 twohop = True
+heads = 4
