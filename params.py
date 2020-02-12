585f75748a8d8d26c425a4531d0d3ef83e4b1e59
coukos <dimitri.coukos@epfl.ch>
2020-02-12 16:17:11 +0100

    update runs

--- params.py
+++ params.py
@@ -1,8 +1,8 @@
 # REMEMBER TO CHANGE VERSION NUMBER
 from models import TwoConv, ThreeConv, SixConv, SixConvResidual, TwentyConv
 
-batch_size = 20
-test_batch_size = 20
+batch_size = 10
+test_batch_size = 10
 validation_split = .10
 shuffle_dataset = False
 random_seed = 37
@@ -14,8 +14,8 @@ lr_decay = 0.3
 lr_cap_decay = 0.7
 patience = 10
 weight_decay = 0
-epochs = 500
-version = '4a'
+epochs = 200
+version = '17b'
 suppress_warnings = True
 dataset = 'thous'
 model_type = SixConv
