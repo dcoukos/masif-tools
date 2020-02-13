db9ea35d7d308eaa4a993e968b2c7d31d8f9a6ae
Dimitri Coukos <dimitri.coukos@epfl.ch>
2020-02-13 17:57:06 +0100

    bugfix

--- models.py
+++ models.py
@@ -427,7 +427,7 @@ class FourConvPoolBlock(torch.nn.Module):
         x = self.conv2(x, edge_index)
         x = x.relu()
         x = self.conv3(x, edge_index)
-        x = self.pool(x)
+        x = self.pool(x, edge_index)
         x = self.conv4(x, edge_index)
         x = x.relu()
         x = self.batch(x)
