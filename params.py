275ff20cf27015ac565d501c42a29cc39021a806
coukos <dimitri.coukos@epfl.ch>
2020-02-14 17:16:39 +0100

    update runs

--- params.py
+++ params.py
@@ -1,5 +1,5 @@
 # REMEMBER TO CHANGE VERSION NUMBER
-from models import ThreeConvBlock
+from models import ThreeConvBlock, TwentyConvNoRes
 
 batch_size = 5
 test_batch_size = 5
@@ -14,11 +14,11 @@ lr_decay = 0.3
 lr_cap_decay = 0.7
 patience = 10
 weight_decay = 0
-epochs = 50
-version = '20a'
+epochs = 2
+version = '19b'
 suppress_warnings = True
 dataset = 'thous'
-model_type = None
+model_type = TwentyConvNoRes
 interface_weight = .8
 twohop = True
 heads = 4
