3aa045993a8e94d9bde118f42cc900acda012dc9
coukos <dimitri.coukos@epfl.ch>
2020-02-17 13:32:38 +0100

    add dataset tracking --cluster

--- params.py
+++ params.py
@@ -1,10 +1,10 @@
 # REMEMBER TO CHANGE VERSION NUMBER
-from models import ThreeConvBlock, TwentyConvNoRes
+from models import ThreeConvBlock, TwentyConvNoRes, PretrainedBlocks
 
 batch_size = 5
 test_batch_size = 5
 validation_split = .10
-shuffle_dataset = True
+shuffle_dataset = False
 random_seed = 37
 dropout = False  # too much dropout?
 batchnorm = True
@@ -15,10 +15,10 @@ lr_cap_decay = 0.7
 patience = 10
 weight_decay = 0
 epochs = 200
-version = '20b_shuffle'
+version = '20b_pos_full'
 suppress_warnings = True
-dataset = 'thous'
-model_type = TwentyConvNoRes
+dataset = 'full'
+model_type = PretrainedBlocks
 interface_weight = .8
 twohop = True
 heads = 4
