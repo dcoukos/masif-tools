206bacfcda8f7cb9e10aa34b66cdc00b6f4aaa26
Dimitri Coukos <dimitri.coukos@epfl.ch>
2020-02-20 14:31:34 +0100

    implement convert_data_for_dataset

--- dataset.py
+++ dataset.py
@@ -48,16 +48,41 @@ def convert_data(path_to_raw='./structures/', n=None, prefix='full'):
     torch.save(train_indices, path_to_output+'{}_indices.pt'.format(prefix))
     print('Done.')
 
-train_structures = Structures('./datasets/thous_train/')
-train_indices = torch.load('./datasets/full_test/raw/full_indices.pt')
-struct_iter = iter(train_structures)
-ex = next(struct_iter)
-name = train_indices[0][1]
-name
-cat = torch.cat((ex.pos, ex.norm, ex.x, ex.shape_index, ex.y), dim=1)
-ex
+
+def convert_data_for_dataset(path_to_raw='./structures/', n=None, prefix='full'):
+    '''
+    Like convert_data converts structures from ply files into pytorch files. Unlike convert_data,
+    each structure gets it's own file. For use with the StructuresDataset class.
+    '''
+        if n is None:
+            t = None
+        else:
+            t = int(n/5)
+        path_to_output = './datasets/{}_test_ds/raw/'.format(prefix)
+        test_indices = []
+        idx = 0
+        print('Saving test structures to file as pytorch object ...')
+        for path in tqdm(glob(path_to_raw + '/test/*')[:t], desc='Reading Structures'):
+            name = path.rsplit('/', 1)[1].split('.')[0]
+            torch.save(read_ply(path), path_to_output+'{}_structure_{}.pt'.format(prefix, idx))
+            test_indices.append((idx, name))
+            idx += 1
+        torch.save(test_structures, path_to_output+'{}_structures.pt'.format(prefix))
+        torch.save(test_indices, path_to_output+'{}_indices.pt'.format(prefix))
+        path_to_output = './datasets/{}_train_ds/raw/'.format(prefix)
+        train_indices = []
+        idx = 0
+        print('Saving train structures to file as pytorch object ...')
+        for path in tqdm(glob(path_to_raw + '/train/*')[:n], desc='Reading Structures'):
+            name = path.rsplit('/', 1)[1].split('.')[0]
+            torch.save(read_ply(path), path_to_output+'{}_structure_{}.pt'.format(prefix, idx))
+            train_indices.append((idx, name))
+            idx += 1
+        print('Done.')
+
 
 def generate_numpy_from_structures(prefix='full'):
+    # TODO: document and update from playground functions..
     train_structures = Structures(root='./datasets/{}_train'.format(prefix))
     train_indices = torch.load('./datasets/{}_train/raw/{}_indices.pt'.format(prefix))
     collection = []
@@ -71,6 +96,7 @@ def generate_numpy_from_structures(prefix='full'):
     test_indices = torch.load('./datasets/{}/raw/{}_indices.pt'.format(prefix))
     collection = []
     for data in tqdm(test_structures, desc='Converting test structures -> numpy'):
+        name = train_indices[idx][1]
         cat = torch.cat((data.pos, data.norm, data.x, data.shape_index, data.y), dim=1).numpy()
         collection.append(cat)
     test_array = np.asarray(collection)
@@ -138,7 +164,7 @@ def read_ply(path, learn_iface=True):
 
     y = [torch.tensor(data['vertex']['iface'])]
     y = torch.stack(y, dim=-1)
-    
+
     face = None
     if 'face' in data:
         faces = data['face']['vertex_indices']
@@ -184,6 +210,7 @@ class Structures(InMemoryDataset):
         self.prefix = p.dataset
         super(Structures, self).__init__(root, transform, pre_transform)
         self.data, self.slices = torch.load(self.processed_paths[0])
+        self.has_nan = []
 
     @property
     def raw_file_names(self):
@@ -209,6 +236,50 @@ class Structures(InMemoryDataset):
         try:
             if data_list[0].shape_index is not None:
                 _, idx = has_nan(data_list)
+                self.has_nan = idx
+                data_list = [data_list[i] for i in range(0, len(data_list)) if i not in idx]
+        except AttributeError:
+            pass
+
+        data, slices = self.collate(data_list)
+        torch.save((data, slices), self.processed_paths[0])
+
+
+class StructuresDataset(Dataset):
+    '''
+    Structures class for datasets that do not fit into memory.
+    '''
+    def __init__(self, root='./datasets/{}/'.format(p.dataset), pre_transform=None, transform=None):
+        self.prefix = p.dataset
+        super(Structures, self).__init__(root, transform, pre_transform)
+        self.data, self.slices = torch.load(self.processed_paths[0])
+        self.has_nan = []
+
+    @property
+    def raw_file_names(self):
+        return ['{}structures.pt'.format(self.prefix + '_')]
+
+    @property
+    def processed_file_names(self):
+        return ['{}structures.pt'.format(self.prefix + '_')]
+
+    def download(self):
+        pass
+
+    def process(self):
+        from utils import has_nan
+        data_list = torch.load(self.raw_paths[0])
+
+        if self.pre_filter is not None:
+            data_list = [data for data in data_list if self.pre_filter(data)]
+
+        if self.pre_transform is not None:
+            data_list = [self.pre_transform(data) for data in tqdm(data_list)]
+
+        try:
+            if data_list[0].shape_index is not None:
+                _, idx = has_nan(data_list)
+                self.has_nan = idx
                 data_list = [data_list[i] for i in range(0, len(data_list)) if i not in idx]
         except AttributeError:
             pass
