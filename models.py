6168e4dd11cf1c384865a4325ff421b2061d0860
Dimitri Coukos <dimitri.coukos@epfl.ch>
2020-02-27 13:43:29 +0100

    model import bugfix

--- models.py
+++ models.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn.functional as F
 from torch.nn import Linear, Dropout, Sequential, ReLU, SELU
 from torch_geometric.nn import GCNConv, FeaStConv, MessagePassing, knn_graph, BatchNorm, TopKPooling, graclus, max_pool, knn_interpolate
-import params as p
 from torch_geometric.data import Data
 # graclus, avg_pool_x
 
@@ -520,125 +519,6 @@ class ThreeConv(torch.nn.Module):
 
         return x
 
-'''
-class SixConv(torch.nn.Module):
-    # Seems underpowered, but less epoch-to-epoch variance in prediction compared to BasicNet
-    # Quick Setup: back to back with max pool and pass through?
-
-    # TODO: confirm that linear layers defined below are functionally equivalent to 1x1 conv
-
-    def __init__(self, n_features, heads=4, dropout=True):
-        super(SixConv, self).__init__()
-        self.conv1 = FeaStConv(n_features, 16, heads=heads)
-        torch.nn.init.xavier_uniform(self.conv1.weight)
-        self.conv2 = FeaStConv(16, 16, heads=heads)
-        torch.nn.init.xavier_uniform(self.conv2.weight)
-        self.conv3 = FeaStConv(16, 16, heads=heads)
-        torch.nn.init.xavier_uniform(self.conv3.weight)
-        self.conv4 = FeaStConv(16, 16)
-        torch.nn.init.xavier_uniform(self.conv4.weight)
-        self.conv5 = FeaStConv(16, 16)
-        torch.nn.init.xavier_uniform(self.conv5.weight)
-        self.conv6 = FeaStConv(16, 16)
-        torch.nn.init.xavier_uniform(self.conv6.weight)
-        self.lin1 = Linear(16, 64)
-        self.lin2 = Linear(64, 64)
-        self.lin3 = Linear(64, 16)
-        self.out = Linear(16, 1)
-        self.batch1 = BatchNorm(16)
-        self.batch2 = BatchNorm(16)
-
-    def forward(self, data):
-        x, edge_index = data.x, data.edge_index
-        x = self.conv1(x, edge_index)
-        x = x.relu()
-        x = self.conv2(x, edge_index)
-        x2 = x
-        x = x.relu()
-        x = self.conv3(x, edge_index)
-        x = self.batch1(x)
-        x = x.relu()
-        x = self.conv4(x, edge_index)
-        x = x.relu()
-        x4 = x
-        x = self.conv5(x, edge_index)
-        x = x.relu()
-        x = self.conv6(x, edge_index)
-        x = self.batch2(x)
-        x = x.relu()
-        x = x + x2 + x4
-        x = self.lin1(x)
-        x = x.relu()
-        x = self.lin2(x)
-        x = x.relu()
-        x = self.lin3(x)
-        x = x.relu()
-        x = self.out(x)
-        x = torch.sigmoid(x)
-
-        return x
-'''
-
-class SixConvResidual(torch.nn.Module):
-    def __init__(self, n_features, heads=4):
-        super(SixConvResidual, self).__init__()
-        self.conv1 = FeaStConv(n_features, 16, heads=heads)
-        torch.nn.init.xavier_uniform(self.conv1.weight)
-        self.conv2 = FeaStConv(16+n_features, 32, heads=heads)
-        torch.nn.init.xavier_uniform(self.conv2.weight)
-        self.conv3 = FeaStConv(48 + n_features, 32, heads=heads)
-        torch.nn.init.xavier_uniform(self.conv3.weight)
-        self.conv4 = FeaStConv(80 + n_features, 32)
-        torch.nn.init.xavier_uniform(self.conv4.weight)
-        self.conv5 = FeaStConv(112 + n_features, 32)
-        torch.nn.init.xavier_uniform(self.conv5.weight)
-        self.conv6 = FeaStConv(144 + n_features, 64)
-        torch.nn.init.xavier_uniform(self.conv6.weight)
-        self.lin1 = Linear(208 + n_features, 256)
-        self.lin2 = Linear(256, 64)
-        self.lin3 = Linear(64, 16)
-        self.out = Linear(16, 1)
-        self.batch1 = BatchNorm(32)
-        self.batch2 = BatchNorm(32)
-        self.batch3 = BatchNorm(208+n_features)
-
-    def forward(self, data):
-        in_, edge_index = data.x, data.edge_index
-        x1 = self.conv1(in_, edge_index)
-        x1 = x1.relu()
-        cat0 = torch.cat((x1, in_), dim=1)
-        x2 = self.conv2(cat0, edge_index)
-        x2 = self.batch1(x2) if p.batchnorm else x2
-        x2 = x2.relu()
-        cat1 = torch.cat((cat0, x2), dim=1)
-        x3 = self.conv3(cat1, edge_index)
-        x3 = x3.relu()
-        cat2 = torch.cat((cat1, x3), dim=1)
-        x4 = self.conv4(cat2, edge_index)
-        x4 = self.batch2(x4) if p.batchnorm else x4
-        x4 = x4.relu()
-        cat3 = torch.cat((cat2, x4), dim=1)
-        x5 = self.conv5(cat3, edge_index)
-        x5 = x5.relu()
-        cat4 = torch.cat((cat3, x5), dim=1)
-        x6 = self.conv6(cat4, edge_index)
-        z = torch.cat((cat4, x6), dim=1)
-        z = self.batch3(z) if p.batchnorm else z
-        z = z.relu()
-        z = self.lin1(z)
-        z = self.dropout(z) if p.dropout else z
-        z = z.relu()
-        z = self.lin2(z)
-        z = self.dropout(z) if p.dropout else z
-        z = z.relu()
-        z = self.lin3(z)
-        z = self.dropout(z) if p.dropout else z
-        z = z.relu()
-        z = self.out(z)
-        z = torch.sigmoid(z)
-
-        return z
-
 
 class TwentyConv(torch.nn.Module):
 
@@ -762,31 +642,6 @@ class TwentyConvPool(torch.nn.Module):
 
         return z
 
-'''
-class FourConv(torch.nn.Module): # Model 18
-
-    def __init__(self, n_features, heads=4):
-        super(FourConv, self).__init__()
-        self.block1 = FourConvBlock(4, 4)
-        self.lin1 = Linear(4, 64)
-        self.lin2 = Linear(64, 64)
-        self.lin3 = Linear(64, 16)
-        self.out = Linear(16, 1)
-
-    def forward(self, data):
-        z, edge_index = data.x, data.edge_index
-        z = self.block1(z, edge_index)
-        z = self.lin1(z)
-        z = z.relu()
-        z = self.lin2(z)
-        z = z.relu()
-        z = self.lin3(z)
-        z = z.relu()
-        z = self.out(z)
-        z = torch.sigmoid(z)
-
-        return z
-'''
 
 class EightConv(torch.nn.Module): # Model 19
 
