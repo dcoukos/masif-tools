d4fbdc1fdece76f1cc16e4897854c33b175c18f1
coukos <dimitri.coukos@epfl.ch>
2020-04-01 13:28:54 +0200

    update runs

--- baseline.py
+++ baseline.py
@@ -49,7 +49,7 @@ if p.shuffle_dataset:
 n_features = trainset.get(0).x.shape[1]
 
 # ---- Import previous model to allow deep network to train -------------
-model = p.model_type(4).to(device)
+model = p.model_type(4, relu=p.relu).to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=learn_rate, weight_decay=p.weight_decay)
 
 writer = SummaryWriter(comment='model:{}_lr:{}_shuffle:{}_seed:{}'.format(
